5414ea75b10dea189e39761f07916d09d7e9637b
Jacky-Summer <1106619871@qq.com>
2020-10-12 11:56:24 +0800

    ci: modify deploy config

--- .umirc.ts
+++ .umirc.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  base: '/monki-ui',
   publicPath: '/monki-ui/',
   mode: 'site',
   exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
--- index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const App = () => <div>123</div>
-
-export default App
