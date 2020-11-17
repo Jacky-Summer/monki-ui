c2e9784c51e33cc45fbad577ff2d0d66039997f0
Jacky-Summer <1106619871@qq.com>
2020-11-17 22:14:53 +0800

    chore: build alert component folder structure

---
+++ components/alert/alert.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react'
+import classNames from 'classnames'
+
+interface Props {}
+
+const Alert: FC<Props> = () => <div>ALert</div>
+
+export default Alert
---
+++ components/alert/index.md
@@ -0,0 +1,8 @@
+---
+title: Alert
+group:
+  title: Alert
+nav:
+  title: '组件'
+  path: /components
+---
